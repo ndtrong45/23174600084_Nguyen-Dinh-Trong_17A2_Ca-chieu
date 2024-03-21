a0382b7b4a5613b57235f60cff34bfec1792327d
44_TRONG_17A2KHDL <149303283+ndtrong45@users.noreply.github.com>
2024-03-21 21:56:43 +0700

    Create bai1.py

---
+++ Lab2/file.py/bai1.py
@@ -0,0 +1,7 @@
+a=float(input("Nhập vào hệ số a: "))
+b=float(input("Nhập vào hệ số b: "))
+if a == 0:
+    print("Phương trình vô nghiệm")
+else:
+    x=-b/a
+    print("Nghiệm của phương trình bậc nhất là:",x)
