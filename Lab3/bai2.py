8a142938fded8fd58dddccd4747e7ce94b2a8261
44_TRONG_17A2KHDL <149303283+ndtrong45@users.noreply.github.com>
2024-03-28 22:26:50 +0700

    Create bai2.py

---
+++ Lab3/bai2.py
@@ -0,0 +1,10 @@
+s=0
+for i in range(2000,4001):
+    if i % 7 == 0 and i % 5 != 0:
+        s+=i 
+print("Tổng các số chia hết cho 7 nhưng không chia hết cho 5 trong khoảng từ 2000 đến 4000 là:",s)
+s1=0
+for i in range(500,1001):
+    if i % 4 == 0 and i % 6 != 0:
+        s1+=i 
+print("Tổng các số chia hết cho 4 nhưng không chia hết cho 6 trong khoảng từ 500 đến 1000 là:",s1)
